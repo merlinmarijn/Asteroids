33fe343758eb29c7f292bc57294da27ff6c0c14b
merlinmarijn <ratched10@hotmail.nl>
2019-10-25 13:09:46 +0200

    Screen/Border changes
    -Increased border size to the sides
    -Added a screenresolution script to force the game to run at a specific aspect ratio (5:4)

--- Assets/Scripts/BorderCheck.cs
+++ Assets/Scripts/BorderCheck.cs
@@ -6,21 +6,21 @@ public class BorderCheck : MonoBehaviour
 {
     void Update()
     {
-        if (gameObject.transform.position.y >= 5.3)
+        if (gameObject.transform.position.y >= 5.2f)
         {
-            gameObject.transform.position = new Vector3(gameObject.transform.position.x, -5.3f, gameObject.transform.position.z);
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x, -5.2f, gameObject.transform.position.z);
         }
-        else if (gameObject.transform.position.y <= -5.3)
+        else if (gameObject.transform.position.y <= -5.2f)
         {
-            gameObject.transform.position = new Vector3(gameObject.transform.position.x, 5.3f, gameObject.transform.position.z);
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x, 5.2f, gameObject.transform.position.z);
         }
-        if (gameObject.transform.position.x >= 5.3)
+        if (gameObject.transform.position.x >= 6.3)
         {
-            gameObject.transform.position = new Vector3(-5.3f, gameObject.transform.position.y, gameObject.transform.position.z);
+            gameObject.transform.position = new Vector3(-6.3f, gameObject.transform.position.y, gameObject.transform.position.z);
         }
-        else if (gameObject.transform.position.x <= -5.3)
+        else if (gameObject.transform.position.x <= -6.3)
         {
-            gameObject.transform.position = new Vector3(5.3f, gameObject.transform.position.y, gameObject.transform.position.z);
+            gameObject.transform.position = new Vector3(6.3f, gameObject.transform.position.y, gameObject.transform.position.z);
         }
     }
 }
