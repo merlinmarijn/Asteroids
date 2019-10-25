5d9a38c228790ef62414c05ec25ae2068ec0176d
merlinmarijn <ratched10@hotmail.nl>
2019-10-25 11:53:13 +0200

    Enemy
    -Added Enemy sprite/Prefab

---
+++ Assets/Scripts/EnemyShip.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShip : MonoBehaviour
+{
+    public Rigidbody2D ship;
+    public float AccelerationForce = 10f;
+    public float RotationForce = 3f;
+    public int Health;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ship.AddTorque(rotation * -RotationForce);
+        ship.AddForce(transform.up * AccelerationForce);
+    }
+}
